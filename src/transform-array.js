165455a9540e1c8a53809dd57b01ec09aaffc5a4
AndreiChuyanov <achuyanov@gmail.com>
2020-02-18 17:36:07 +0300

    Update transform-array.js
    feat: add solution for arrays transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,32 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if (!Array.isArray(arr)) throw new Error();
+    
+    let len = arr.length-1;
+    let ret = [];
+
+    arr.forEach( (val, i) => {
+        switch (val) {
+            case '--discard-prev':
+                if (i > 0) ret.pop();
+                break;
+
+            case '--double-prev':
+                if (i > 0) ret.push(arr[i - 1]);
+                break;
+
+            case '--double-next' :
+                if (len > i) ret.push(arr[i + 1]);
+                break;
+
+            case '--discard-next':
+                //
+                break;
+
+            default :
+                if (arr[i - 1] != '--discard-next') ret.push(val);
+                break;
+        }
+    });
+    return ret;
 };
