95cf6e628edd6dfbb84b536ae3de9258749efe1e
AndreiChuyanov <achuyanov@gmail.com>
2020-02-23 00:25:00 +0300

    feat: add crypt table

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,25 @@
 class VigenereCipheringMachine {
-    encrypt() {
+    
+    constructor( direct = true ){
+        this.table = new Array(26).fill(new Array(26).fill('').map((v,i) =>String.fromCharCode(65+i))).map((a,i)=> a.slice(i).concat(a.slice(0,i)));
+        this.direct = direct;  
+    }
+    
+    createKeyString(str,key) {
+        return key.repeat(Math.ceil(str.length/key.length));
+    } 
+
+    encrypt(str, key, isDirect) {
         throw 'Not implemented';
         // remove line with error and write your code here
     }
 
-    decrypt() {
+    decrypt(str, key, isDirect) {
         throw 'Not implemented';
         // remove line with error and write your code here
     }
+
+    crypt(input, key, isDirect)
 }
 
 module.exports = VigenereCipheringMachine;
